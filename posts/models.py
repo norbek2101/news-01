2adf6a644aa8833ea7de3557fce6184474099002
norbek <lightnight1223@gmail.com>
2021-06-15 21:20:21 +0500

    posts app da o'zgarishlar bo'ldi

--- posts/models.py
+++ posts/models.py
@@ -1,4 +1,10 @@
 from django.db import models
 
 class Post(models.Model):
+    title = models.CharField(max_length=200, null=True)
     text = models.TextField()
+    author = models.ForeignKey(
+        'auth.User',
+        on_delete=models.CASCADE,
+    )
+
